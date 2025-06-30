a560d32ec878151eac9deca8a9bcf5fa74a8a083
Sajjad Ghanbari <a.s.ghanbari.j@gmail.com>
2025-06-30 22:19:55 +0330

    delete task created

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,19 @@ import TaskList from './components/TaskList';
 
 function App() {
   const [tasks, setTasks] = useState([])
-const addTask = (task) => {
+  const addTask = (task) => {
     setTasks(prevState => [...prevState, task])
-};
+  };
+  const deleteTask = (id) => {
+    setTasks(prevState => prevState.filter(t => t.id !== id))
+  }
   return (
     <div className='container'>
       <header>
         <h1>My Task List</h1>
       </header>
       <CustomForm addTask={addTask} />
-    {tasks && <TaskList tasks={tasks} />}
+      {tasks && (<TaskList tasks={tasks} deleteTask={deleteTask} />)}
 
     </div>
   )
--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -3,7 +3,7 @@ import styles from './TaskList.module.css';
 import { CheckIcon } from '@heroicons/react/24/outline';
 import { PencilIcon } from '@heroicons/react/24/outline';
 import { TrashIcon } from '@heroicons/react/24/solid';
-const TaskItem = ({ task }) => {
+const TaskItem = ({ task , deleteTask}) => {
     const [isChecked, setIsChecked] = useState(task.checked);
     const handleCheckboxChange = (event) => {
         setIsChecked(!isChecked);
@@ -24,7 +24,7 @@ const TaskItem = ({ task }) => {
                 <button className='btn' aria-label={`Update ${task.name} Task`}>
                     <PencilIcon  width={24} height={24} />
                 </button>
-                <button className={`btn ${styles.delete}`} aria-label={`Delete ${task.name} Task`}>
+                <button className={`btn ${styles.delete}`} aria-label={`Delete ${task.name} Task` } onClick={() => deleteTask(task.id)}>
                     <TrashIcon  width={24} height={24} />
                 </button>
             </div>
