6f3c5c2792b90f3d874d486627a843477c1e6870
Sajjad Ghanbari <a.s.ghanbari.j@gmail.com>
2025-06-30 16:44:40 +0330

    displaying tasks in a list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import CustomForm from './components/CustomForm'
+import TaskList from './components/TaskList';
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([])
 const addTask = (task) => {
-    
+    setTasks(prevState => [...prevState, task])
 };
   return (
     <div className='container'>
@@ -13,7 +14,7 @@ const addTask = (task) => {
         <h1>My Task List</h1>
       </header>
       <CustomForm addTask={addTask} />
-
+    {tasks && <TaskList tasks={tasks} />}
 
     </div>
   )
---
+++ src/components/TaskItem.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import styles from './TaskList.module.css';
+import { CheckIcon } from '@heroicons/react/24/outline';
+import { PencilIcon } from '@heroicons/react/24/outline';
+import { TrashIcon } from '@heroicons/react/24/solid';
+const TaskItem = ({ task }) => {
+    const [isChecked, setIsChecked] = useState(task.checked);
+    const handleCheckboxChange = (event) => {
+        setIsChecked(!isChecked);
+    };
+    return (
+        <li className={styles.task}>
+            
+            <div className={styles["task-group"]}>
+                <input type="checkbox" className={styles.checkbox} checked={isChecked} id={task.id} name={task.name} onChange={handleCheckboxChange} />
+                <label htmlFor={task.id} className={styles.label} >
+                    {task.name}
+                    <p className={styles.checkmark}>
+                        <CheckIcon strokeWidth={2} width={24} height={24} />
+                    </p>
+                </label>
+            </div>
+            <div className={styles["task-group"]}>
+                <button className='btn' aria-label={`Update ${task.name} Task`}>
+                    <PencilIcon  width={24} height={24} />
+                </button>
+                <button className={`btn ${styles.delete}`} aria-label={`Delete ${task.name} Task`}>
+                    <TrashIcon  width={24} height={24} />
+                </button>
+            </div>
+        </li>
+        
+    )
+}
+export default TaskItem;
